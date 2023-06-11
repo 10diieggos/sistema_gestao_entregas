3e7d62d74f43448b27766600c7bc71ef020fd7d8
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-11 15:42:39 -0300

    Correção da importação Formulario
    A importação funcionou corretamente apenas com o .js ao final

--- frontend/src/components/SqlConstrutor.js
+++ frontend/src/components/SqlConstrutor.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Formulario from "./sqlconstrutor/Formulario.js";
 
 const SqlConstrutor = () => {
   return (
     <div>
       <h1>Bem-vindo à minha página inicial!</h1>
       <p>Esta é uma página de exemplo.</p>
+      <Formulario />
     </div>
   );
 };
