f86a35ea1a49c994483175c7e7ce208004550074
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-11 20:04:44 -0300

    Atualiza e extrai id_objeto e id_servico
    implementada a funcao que faz update na tanela objeto no campo id_servico e extrai os dois id s.

--- frontend/src/components/sqlconstrutor/extractIdObjetos.js
+++
@@ -1,5 +0,0 @@
-// Arquivo utils.js
-export default function extractIdObjetos(inputText) {
-  // Tratar o valor do inputText para extrair o id_objetos da lista de codigos fornecida
-  return inputText.split(',').map(item => item.trim()).join('\n');
-}
---
+++ frontend/src/components/sqlconstrutor/extrai_id_objeto.js
@@ -0,0 +1,22 @@
+export default function extrai_id_objeto(inputText) {
+    const objetos = inputText.trim().split('\n'); // separa as linhas do conjunto de dados
+    
+    let updateQuery = 'UPDATE Objetos o\nJOIN Servicos s ON SUBSTR(o.codigo, 1, 2) = s.sigla\nSET o.id_servico = s.id WHERE\n';
+    let selectQuery = 'SELECT o.codigo, o.id as id_objeto, o.id_servico\nFROM Objetos o\nJOIN Servicos s ON SUBSTR(o.codigo, 1, 2) = s.sigla\nWHERE\n';
+    
+    // adiciona as cláusulas WHERE para cada código de objeto
+    for (let i = 0; i < objetos.length; i++) {
+      const codigo = objetos[i].trim();
+      if (codigo !== '') { // verifica se o código não está vazio
+        updateQuery += `o.codigo = '${codigo}' OR\n`;
+        selectQuery += `o.codigo = '${codigo}' OR\n`;
+      }
+    }
+    
+    // remove a última cláusula OR de cada consulta e adiciona um ponto-e-vírgula
+    updateQuery = updateQuery.slice(0, -3) + ';';
+    selectQuery = selectQuery.slice(0, -3) + ';';
+    
+    // retorna as consultas concatenadas em uma única string
+    return updateQuery + '\n\n' + selectQuery;
+  }
