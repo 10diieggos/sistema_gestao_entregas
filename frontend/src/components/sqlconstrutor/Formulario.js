b1f385148a0a885e695c9319deb2746b0cd7668d
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-11 17:28:28 -0300

    Valor tranferido entre os textarea
    Capturado os valores nas variaveis. Valor tratado do textarea 1 é colado no textarea 2 apos clicar o botao

--- frontend/src/components/sqlconstrutor/Formulario.js
+++ frontend/src/components/sqlconstrutor/Formulario.js
@@ -1,43 +1,72 @@
 import React from 'react';
 import { FormControl, FormLabel, Radio, RadioGroup, FormControlLabel, Button } from '@mui/material';
+import { TextareaAutosize } from '@mui/base';
 
 class Formulario extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       selectedOption: '',
+      inputText: '',
+      outputText: '',
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.handleRadioChange = this.handleRadioChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(event) {
+  handleRadioChange(event) {
     this.setState({
       selectedOption: event.target.value,
     });
   }
 
+  handleInputChange(event) {
+    this.setState({
+      inputText: event.target.value,
+    });
+  }
+
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state.selectedOption);
+    console.log(this.state.selectedOption)
+    // Tratar o valor do inputText aqui
+    const treatedValue = this.state.inputText.toUpperCase();
+    // Atualizar o estado do outputText com o valor tratado
+    this.setState({
+      outputText: treatedValue,
+    });
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
+         
         <FormControl>
-          <FormLabel id="demo-radio-buttons-group-label">Escolha a consulta desejada</FormLabel>
+          <FormLabel id="demo-radio-buttons-group-label">Escolha a operação desejada</FormLabel>
           <RadioGroup
             aria-labelledby="demo-radio-buttons-group-label"
             name="radio-buttons-group"
             value={this.state.selectedOption}
-            onChange={this.handleChange}
+            onChange={this.handleRadioChange}
           >
             <FormControlLabel value="id_objetos" control={<Radio />} label="Extrair o id_objetos da lista de codigos fornecida" />
             <FormControlLabel value="opcao2" control={<Radio />} label="opcao2" />
-            <Button variant="contained" type="submit">Contained</Button>
           </RadioGroup>
+          <Button variant="contained" type="submit">Contained</Button>
         </FormControl>
+          <TextareaAutosize 
+            minRows={50} 
+            maxRows="60"
+            value={this.state.inputText}
+            onChange={this.handleInputChange}
+          />     
+          <TextareaAutosize 
+            minRows={50} 
+            maxRows="60"
+            value={this.state.outputText}
+            readOnly
+          />     
       </form>
     );
   }
