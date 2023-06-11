6925fc6476b4f5b893c58c64f7bf2f712a74179f
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-11 15:07:39 -0300

    Primeira Rota Funcional
    testada e aprovada a rota /sqlconstrutor

---
+++ frontend/src/components/SqlConstrutor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SqlConstrutor = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à minha página inicial!</h1>
+      <p>Esta é uma página de exemplo.</p>
+    </div>
+  );
+};
+
+export default SqlConstrutor;
