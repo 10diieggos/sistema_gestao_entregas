84f39367ffafd8f4cf2299bd3e8f46145dc8a02e
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-13 08:27:53 -0300

    Ajustando para o quarto prototipo

--- dados/modelagem_fisica_sge.sql
+++ dados/modelagem_fisica_sge.sql
@@ -18,6 +18,9 @@ CREATE TABLE Objetos (
   id INT PRIMARY KEY AUTO_INCREMENT,
   codigo CHAR(13) NOT NULL,
   ordem INT DEFAULT NULL,
+  destinatario VARCHAR(100) NOT NULL,
+  endereco VARCHAR(100) DEFAULT NULL,
+  num_endereco INT DEFAULT NULL,
   distribuicao ENUM('E', 'I')  DEFAULT 'I',
   duplicado TINYINT(1) DEFAULT 0,
   id_servico INT,
@@ -26,52 +29,6 @@ CREATE TABLE Objetos (
   tentativas_restantes INT DEFAULT NULL,
   UNIQUE (codigo),
   FOREIGN KEY (id_servico) REFERENCES Servicos(id)
-);
-CREATE TABLE Recebedores (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  cpf CHAR(11),
-  nome VARCHAR(100) NOT NULL,
-  UNIQUE (cpf)
-);
-CREATE TABLE objetos_recebedores (
-  id_objeto INT,
-  id_recebedor INT,
-  formal TINYINT(1) DEFAULT NULL,
-  PRIMARY KEY (id_objeto, id_recebedor),
-  FOREIGN KEY (id_objeto) REFERENCES Objetos(id),
-  FOREIGN KEY (id_recebedor) REFERENCES Recebedores(id)
-);
-CREATE TABLE Enderecos (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  logradouro VARCHAR(100) DEFAULT NULL,
-  numero INT DEFAULT NULL,
-  existe_numero TINYINT(1) DEFAULT NULL,
-  previsao_entrega_externa TINYINT(1) DEFAULT NULL,
-  municipio VARCHAR(100) DEFAULT NULL,
-  estado CHAR(2) DEFAULT NULL,
-  bairro VARCHAR(100) DEFAULT NULL,
-  CEP CHAR(8) DEFAULT NULL
-);
-CREATE TABLE objetos_enderecos (
-  id_objeto INT,
-  id_endereco INT,
-  PRIMARY KEY (id_objeto, id_endereco),
-  FOREIGN KEY (id_objeto) REFERENCES Objetos(id),
-  FOREIGN KEY (id_endereco) REFERENCES Enderecos(id)
-);
-CREATE TABLE Destinatarios (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  cpf CHAR(11),
-  nome VARCHAR(100) NOT NULL,
-  telefone CHAR(11) DEFAULT NULL
-);
-
-CREATE TABLE objetos_destinatarios (
-  id_objeto INT,
-  id_destinatario INT,
-  PRIMARY KEY (id_objeto, id_destinatario),
-  FOREIGN KEY (id_objeto) REFERENCES Objetos(id),
-  FOREIGN KEY (id_destinatario) REFERENCES Destinatarios(id)
 );
 CREATE TABLE Listas (
   id INT PRIMARY KEY AUTO_INCREMENT,
@@ -88,16 +45,66 @@ CREATE TABLE objetos_listas (
   FOREIGN KEY (id_objeto) REFERENCES Objetos(id),
   FOREIGN KEY (id_lista) REFERENCES Listas(id)
 );
+CREATE TABLE Recebedores (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  cpf CHAR(11),
+  nome VARCHAR(100) NOT NULL,
+  UNIQUE (cpf)
+);
+CREATE TABLE objetos_recebedores (
+  id_objeto INT,
+  id_recebedor INT,
+  formal TINYINT(1) DEFAULT NULL,
+  PRIMARY KEY (id_objeto, id_recebedor),
+  FOREIGN KEY (id_objeto) REFERENCES Objetos(id),
+  FOREIGN KEY (id_recebedor) REFERENCES Recebedores(id)
+);
+CREATE TABLE Contatos (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  telefone CHAR(11),
+  UNIQUE (telefone)
+);
 CREATE TABLE Eventos (
   id_objeto INT,
   data_hora DATETIME,
-  -- momento_consulta DATETIME DEFAULT NULL,
   local VARCHAR(100) DEFAULT NULL,
   situacao VARCHAR(100) DEFAULT NULL,
   mensagem VARCHAR(200) DEFAULT NULL,
   PRIMARY KEY (id_objeto, data_hora),
   FOREIGN KEY (id_objeto) REFERENCES Objetos(id)
 );
+-- CREATE TABLE Destinatarios (
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   cpf CHAR(11),
+--   nome VARCHAR(100) NOT NULL,
+--   telefone CHAR(11) DEFAULT NULL
+-- );
+-- CREATE TABLE objetos_destinatarios (
+--   id_objeto INT,
+--   id_destinatario INT,
+--   PRIMARY KEY (id_objeto, id_destinatario),
+--   FOREIGN KEY (id_objeto) REFERENCES Objetos(id),
+--   FOREIGN KEY (id_destinatario) REFERENCES Destinatarios(id)
+-- );
+-- CREATE TABLE Enderecos (
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   logradouro VARCHAR(100) DEFAULT NULL,
+--   numero INT DEFAULT NULL,
+--   existe_numero TINYINT(1) DEFAULT NULL,
+--   previsao_entrega_externa TINYINT(1) DEFAULT NULL,
+--   municipio VARCHAR(100) DEFAULT NULL,
+--   estado CHAR(2) DEFAULT NULL,
+--   bairro VARCHAR(100) DEFAULT NULL,
+--   CEP CHAR(8) DEFAULT NULL
+-- );
+-- CREATE TABLE objetos_enderecos (
+--   id_objeto INT,
+--   id_endereco INT,
+--   PRIMARY KEY (id_objeto, id_endereco),
+--   FOREIGN KEY (id_objeto) REFERENCES Objetos(id),
+--   FOREIGN KEY (id_endereco) REFERENCES Enderecos(id)
+-- );
+
 -- CREATE TABLE destinatarios_contatos (
 --   id_destinatario INT,
 --   id_contato INT,
