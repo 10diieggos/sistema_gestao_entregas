8877bf618b9634737903461790d4ae9b5252e841
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-16 12:58:33 -0300

    Update insere_servico.js
    Clausula ON DUPLICATE KEY adicionada para evitar erros na tentativa de inserir evento duplicado

--- frontend/src/components/sqlconstrutor/insere_servico.js
+++ frontend/src/components/sqlconstrutor/insere_servico.js
@@ -1,5 +1,5 @@
 export default function insere_servico(inputText) {
-  let query = "INSERT INTO Servicos (prazo_guarda_interna, admite_residuo, tentativas_externas_previstas, sigla, descricao, categoria, familia, gera_pre_alerta, hora_real_entrega, dados_do_recebedor_na_baixa, entrega_externa, entrega_com_imagem) VALUES\n";
+  let query = "INSERT INTO Servicos\n (prazo_guarda_interna,\n admite_residuo,\n tentativas_externas_previstas,\n sigla,\n descricao,\n categoria,\n familia,\n gera_pre_alerta,\n hora_real_entrega,\n dados_do_recebedor_na_baixa,\n entrega_externa,\n entrega_com_imagem) VALUES\n";
 
   for (let linha of inputText.split("\n")) {
     if (linha.trim() === "") continue;
@@ -10,5 +10,5 @@ export default function insere_servico(inputText) {
     query += "(" + valores + "),\n";
   }
 
-  return query.slice(0, -2) + ";";
+  return query.slice(0, -2) + "ON DUPLICATE KEY UPDATE prazo_guarda_interna = VALUES(prazo_guarda_interna), admite_residuo = VALUES(admite_residuo), tentativas_externas_previstas = VALUES(tentativas_externas_previstas), sigla = VALUES(sigla), descricao = VALUES(descricao), categoria = VALUES(categoria), familia = VALUES(familia), gera_pre_alerta = VALUES(gera_pre_alerta), hora_real_entrega = VALUES(hora_real_entrega), dados_do_recebedor_na_baixa = VALUES(dados_do_recebedor_na_baixa), entrega_externa = VALUES(entrega_externa), entrega_com_imagem = VALUES(entrega_com_imagem);";
 }
