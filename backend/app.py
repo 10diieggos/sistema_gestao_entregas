184b2641dd1beccd990eb77e00c440ef1699e27e
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-20 08:16:31 -0300

    Primeiro Exemplo Funcional
    Neste teste foi realizada a primeira requisição bem sucedida entre a API e o banco de dados. A API retornou os dados da tabela listas

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
-# local deste arquivo: backend/app.py
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from flask_restx import Api, Resource, fields
+from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -15,37 +18,51 @@ db_user = os.getenv('MYSQL_USER')
 db_password = os.getenv('MYSQL_PASSWORD')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_user}:{db_password}@localhost:{db_port}/{db_name}'
 db = SQLAlchemy(app)
 
-# Exemplo simples de rota
-class Objeto(db.Model):
-    __tablename__ = 'Objetos'
+# Cria o objeto api do Flask-RESTX e associa ao objeto app do Flask
+api = Api(app)
 
-    id = db.Column(db.Integer, primary_key=True)
-    codigo = db.Column(db.String(13), unique=True, nullable=False)
-    ordem = db.Column(db.Integer)
-    destinatario = db.Column(db.String(100), nullable=False)
-    endereco = db.Column(db.String(100))
-    num_endereco = db.Column(db.Integer)
-    distribuicao = db.Column(db.Enum('E', 'I'), default='I')
-    duplicado = db.Column(db.Boolean, default=False)
-    id_servico = db.Column(db.Integer, db.ForeignKey('Servicos.id'))
-    disponivel = db.Column(db.Boolean)
-    pendencia_baixa = db.Column(db.Boolean)
-    tentativas_restantes = db.Column(db.Integer)
+# Define o modelo para a tabela Listas
+listas_model = api.model('ListasModel', {
+    'id': fields.Integer,
+    'data_Hora': fields.DateTime(dt_format='iso8601'),
+    'numero': fields.String,
+    'modalidade': fields.String,
+    'criado': fields.DateTime(dt_format='iso8601'),
+    'atualizado': fields.DateTime(dt_format='iso8601'),
+})
 
-    def __repr__(self):
-        return f'<Objeto {self.codigo}>'
+# Define a rota '/listas' e aplica o modelo aos dados retornados pela função
+@api.route('/listas')
+class ListasResource(Resource):
+    @api.marshal_with(listas_model, envelope='data')
+    def get(self):
+        # Cria uma conexão com o banco de dados
+        conn = db.engine.connect()
 
-@app.route('/')
-def home():
-    return 'Olá, mundo! Esta é a página inicial do meu aplicativo Flask.'
+        # Executa uma consulta SQL na tabela Listas
+        query = text('SELECT * FROM listas')
+        result = conn.execute(query)
 
-@app.route('/objetos')
-def objetos():
-    objetos = Objeto.query.all()
-    return render_template('objetos.html', objetos=objetos)
+        # Converte o resultado em uma lista de dicionários
+        listas = []
+        for row in result:
+            lista = {
+                'id': row.id,
+                'data_Hora': row.data_Hora.isoformat(),
+                'numero': row.numero,
+                'modalidade': row.modalidade,
+                'criado': row.criado.isoformat(),
+                'atualizado': row.atualizado.isoformat(),
+            }
+            listas.append(lista)
+
+        # Fecha a conexão com o banco de dados
+        conn.close()
+
+        return listas
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
