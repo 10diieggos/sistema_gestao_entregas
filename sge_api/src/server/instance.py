7b5725339b1e6af094841ede47966ad18d7d5ca5
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-23 14:52:08 -0300

    CORS. configuracao das restricoes

--- sge_api/src/server/instance.py
+++ sge_api/src/server/instance.py
@@ -1,9 +1,20 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
+
+load_dotenv()
 
 class Server():
     def __init__(self,):
         self.app = Flask(__name__)
+        allowed_origins = os.getenv('ALLOWED_ORIGINS')
+        if allowed_origins:
+            allowed_origins = allowed_origins.split(',')
+        else:
+            allowed_origins = '*'
+        CORS(self.app, origins=allowed_origins)
         self.api = Api(
             self.app,
             version='1.0',
