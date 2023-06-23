679a66cf39996fbfc81f01c26e9028ab3352be79
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-23 14:54:06 -0300

    Rota ATENDIMENTO

--- sge_app/src/App.js
+++ sge_app/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SqlConstrutor from './components/SqlConstrutor';
+import Atendimento from './components/Atendimento';
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/sqlconstrutor" component={SqlConstrutor} />
+        <Route exact path="/sge/atendimento" component={Atendimento} />
       </Switch>
     </Router>
   );
---
+++ sge_app/src/components/Atendimento.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tabela from "./atendimento/Tabela.js";
+
+const Atendimento = () => {
+  return (
+    <div>
+      <Tabela />
+    </div>
+  );
+};
+
+export default Atendimento;
