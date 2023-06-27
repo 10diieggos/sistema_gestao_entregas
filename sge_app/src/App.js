176e4993a44cd2d2fa3f8a766178c901f9f2293f
Diego Richard Lemos <10diieggos@gmail.com>
2023-06-27 08:19:08 -0300

    Navegacao Menu Basico

--- sge_app/src/App.js
+++ sge_app/src/App.js
@@ -1,14 +1,39 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import SqlConstrutor from './components/SqlConstrutor';
 import Atendimento from './components/Atendimento';
+import './App.css';
+
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <Router>
-      <Switch>
-        <Route exact path="/sqlconstrutor" component={SqlConstrutor} />
-        <Route exact path="/sge/atendimento" component={Atendimento} />
-      </Switch>
+      <div className="app">
+        <button className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+          <i className={`fas ${menuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+        </button>
+        <div className={`sidebar ${menuOpen ? 'open' : ''}`}>
+          <ul className="menu">
+            <li>
+              <Link to="/sqlconstrutor" onClick={() => setMenuOpen(false)}>
+                SQL Construtor
+              </Link>
+            </li>
+            <li>
+              <Link to="/sge/atendimento" onClick={() => setMenuOpen(false)}>
+                Atendimento
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="content">
+          <Switch>
+            <Route exact path="/sqlconstrutor" component={SqlConstrutor} />
+            <Route exact path="/sge/atendimento" component={Atendimento} />
+          </Switch>
+        </div>
+      </div>
     </Router>
   );
 }
